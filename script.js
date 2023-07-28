3d2d22ee9be01786bb25e2cf681b6d1733943393
prolixmagus <jeremypfishman@gmail.com>
2023-07-28 14:20:16 -0700

    Functionality Fix Create two loops to create rows and grid squares. Add background image and header.

--- script.js
+++ script.js
@@ -9,27 +9,40 @@ function createCell() {
     return cell;
 }
 
-function createGrid(input) {
-
+function drawGrid(input) {
     let container = document.querySelector('#container');
-        for (j = input * input; j > 0; j--) {
+        for (h = 0; h < input; h++) {
                 let cell = createCell()
                 container.appendChild(cell)
         }
-    }
 
+        const rows = document.querySelectorAll('#cell');
+        rows.forEach((item) => {
+            for (w = 0; w < input; w++) {
+                let square = document.createElement('div')
+                square.setAttribute('id', 'square')
+                item.appendChild(square)
+        }
+    })
+        removeEvent();
+}
+    
 
+function removeEvent() {
+    let gridSizeButton = document.querySelector('#grid-size')
+    gridSizeButton.removeEventListener('click', createGrid)
+}
 //adding events to grid cells
 
 function changeAllCellColor() {
-    let allCells = document.querySelectorAll('#cell')
-    allCells.forEach((item) => {
-        item.addEventListener('mouseover', changeColor)
+    let allSquares = document.querySelectorAll('#square')
+    allSquares.forEach((square) => {
+        square.addEventListener('mouseenter', changeColor)
     })
 }
 
 function changeColor (e) {
-    e.target.style.background = "yellow"
+    e.target.style.background = "cyan"
 }
 
 //button event
@@ -63,26 +76,25 @@ function reset() {
 function userInput() {
     let input = ""
     do {
-        input = prompt('Enter a single digit less than 100 to create a L x W square .\n Hint: The smaller the number, the bigger the pixels')
+        input = Math.floor(+prompt('Enter a single digit less than 100 to create a L x W square .\nHint: The smaller the number, the bigger the pixels'))
     }   while (isNaN(input) || input > 100 || input <= 0)
 
     return input;
 }
 
-function askUser() {
+function askUserForGrid() {
     let gridSizeButton = document.querySelector('#grid-size')
-    gridSizeButton.addEventListener('click', function () {
-        const input = userInput()
-        createGrid(input);
-        changeAllCellColor()
+    gridSizeButton.addEventListener('click', createGrid)
+}
 
-    })
+function createGrid(){ 
+    const input = userInput()
+    drawGrid(input);
+    changeAllCellColor()
 }
 
 //running the program
-askUser();
+
+askUserForGrid();
 buttonChange();
 removeButtonChange();
-
-
-
